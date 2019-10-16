d136e5797fbde7f88048684aefd86b1f99c4042f
Joe Baragona <jrb5004@gmail.com>
2019-10-15 18:36:46 -0700

    updated stylging for responsive design

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -3,6 +3,8 @@ import { Route, Link } from 'react-router-dom'
 import './HomePage.css'
 import ApiContext from '../ApiContext'
 
+var logoOne = require('../Images/Logo.png')
+var logoTwo = require('../Images/cookbook.png')
 
 
 class HomePage extends Component {
@@ -17,24 +19,30 @@ class HomePage extends Component {
       const { categories=[] } = this.context
     return (
       <div className='HomePage'>
-        <section className='Description'>
-          <p>The Cocktail Cookbook provides the professional mixologist, the amatuer house-party bartender, and everyone inbetween, a convenient repository of cocktail recipes organized by category!</p>
-          <p>View and edit recipes, add your own recipes to the collection, and leave comments with your feedback.</p>
+        <main className='outerWrapper'>
+          <section className='Description'>
+            <p>The Cocktail Cookbook provides the professional mixologist, the amatuer house-party bartender, and everyone inbetween, a convenient repository of cocktail recipes organized by category!</p>
+            <div className = 'logosWrapper'>
+              <img src={logoOne} alt = 'cocktail logo' />
+              <img src={logoTwo} alt = 'cookbook logo' />
+            </div>
+            <p>View and edit recipes, add your own recipes to the collection, and leave comments with your feedback.</p>
+          </section>
+          <section className='ViewCocktails'>
+            <h2>View Recipes!</h2>
+            <p>Select cocktail category below to view recipes and start mixin'!</p>
+            <ul className='CatList'>
+              {categories.map(cat =>
+              <li key={cat.id}>
+                  <Link to={`/cat/${cat.id}`}>{cat.name}</Link>
+              </li>
+              )}
+            </ul>
+          </section>
+        </main>
+          <section>
+          <p className="AddRecipeParagraph">Have a great recipe not found in the cookbook?  Add it <Link to={`/addrecipe`}><span className="AddRecipeLink">here</span></Link>.</p>
         </section>
-        <section className='ViewCocktails'>
-          <h2>View Recipes!</h2>
-          <p>Select cocktail category below to view recipes and start mixin'!</p>
-          <ul className='CatList'>
-            {categories.map(cat =>
-            <li key={cat.id}>
-                <Link to={`/cat/${cat.id}`}>{cat.name}</Link>
-            </li>
-            )}
-          </ul>
-        </section>
-        <section>
-        <p className="AddRecipeParagraph">Have a great recipe not found in the cookbook?  Add it <Link to={`/addrecipe`}><span className="AddRecipeLink">here</span></Link>.</p>
-      </section>
       </div>
     )
   }
